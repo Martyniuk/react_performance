d7a6f1c52990cbb432be71b7f5f384c3d08960ae
Martyniuk Vladimir <vl.martyniuk@gmail.com>
2018-02-02 01:15:01 +0200

    additional img of shishas added, styles fixed

--- src/components/Barchik/index.js
+++ src/components/Barchik/index.js
@@ -5,13 +5,20 @@ import React, { Component } from 'react';
 import Styles from './styles.scss';
 import hab from '../../theme/assets/blueHabibi.jpg';
 import amy from '../../theme/assets/amy.jpg';
+import eg1 from '../../theme/assets/eg1.jpeg';
+import egypt from '../../theme/assets/egypt.jpeg';
+import kherson from '../../theme/assets/kherson.jpeg';
+import km from '../../theme/assets/km.jpeg';
+import amy2 from '../../theme/assets/amy2.jpeg';
+import syr from '../../theme/assets/syr.jpeg';
+import lol from '../../theme/assets/lol.jpeg';
 
 // Components
 import Shisha from '../Shisha';
 
 const adjectives = ["adamant", "adroit", "amatory", "animistic", "antic", "arcadian", "baleful", "bellicose", "bilious", "boorish", "calamitous", "caustic", "cerulean", "comely", "concomitant", "contumacious", "corpulent", "crapulous", "defamatory", "didactic", "dilatory", "dowdy", "efficacious", "effulgent", "egregious", "endemic", "equanimous", "execrable", "fastidious", "feckless", "fecund", "friable", "fulsome", "garrulous", "guileless", "gustatory", "heuristic", "histrionic", "hubristic", "incendiary", "insidious", "insolent", "intransigent", "inveterate", "invidious", "irksome", "jejune", "jocular", "judicious", "lachrymose", "limpid", "loquacious", "luminous", "mannered", "mendacious", "meretricious", "minatory", "mordant", "munificent", "nefarious", "noxious", "obtuse", "parsimonious", "pendulous", "pernicious", "pervasive", "petulant", "platitudinous", "precipitate", "propitious", "puckish", "querulous", "quiescent", "rebarbative", "recalcitant", "redolent", "rhadamanthine", "risible", "ruminative", "sagacious", "salubrious", "sartorial", "sclerotic", "serpentine", "spasmodic", "strident", "taciturn", "tenacious", "tremulous", "trenchant", "turbulent", "turgid", "ubiquitous", "uxorious", "verdant", "voluble", "voracious", "wheedling", "withering", "zealous"];
 const nouns = ["ninja", "chair", "pancake", "statue", "unicorn", "rainbows", "laser", "senor", "bunny", "captain", "nibblets", "cupcake", "carrot", "gnomes", "glitter", "potato", "salad", "toejam", "curtains", "beets", "toilet", "exorcism", "stick figures", "mermaid eggs", "sea barnacles", "dragons", "jellybeans", "snakes", "dolls", "bushes", "cookies", "apples", "ice cream", "ukulele", "kazoo", "banjo", "opera singer", "circus", "trampoline", "carousel", "carnival", "locomotive", "hot air balloon", "praying mantis", "animator", "artisan", "artist", "colorist", "inker", "coppersmith", "director", "designer", "flatter", "stylist", "leadman", "limner", "make-up artist", "model", "musician", "penciller", "producer", "scenographer", "set decorator", "silversmith", "teacher", "auto mechanic", "beader", "bobbin boy", "clerk of the chapel", "filling station attendant", "foreman", "maintenance engineering", "mechanic", "miller", "moldmaker", "panel beater", "patternmaker", "plant operator", "plumber", "sawfiler", "shop foreman", "soaper", "stationary engineer", "wheelwright", "woodworkers"];
-const shishasImg = [hab, amy];
+const shishasImg = [hab, amy, eg1, kherson, km, amy2, syr, lol, egypt];
 
 export default class Barchik extends Component {
     constructor () {
